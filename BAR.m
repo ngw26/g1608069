19885f7813c4ff08abfe8331e1e51f04c93b27a4
nworth <nworth@uk-nworth.dhcp.mathworks.com>
2017-07-28 11:23:15 +0100

    Initial check in

---
+++ BAR.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = BAR(inputArg1,inputArg2)
+%BAR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
